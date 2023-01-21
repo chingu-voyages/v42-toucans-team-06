7ff9b8fce1aad3f7c6146c382ed969e112bd50ac
Maikoke <mamadoumaikoke11@gmail.com>
2023-01-21 10:56:37 +0100

    typing effect and anti-spam system

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 const queryElement = document.querySelector(".query")
 const excludeCategories = ["religion", "political", "explicit"]
 const options = createSelect(excludeCategories, queryElement)
+const results = document.querySelector(".results")
+const cursor = document.createElement("span")
+cursor.setAttribute('id', 'cursor')
+
 
   async function createSelect(excludeCategories, domElement) {
     const response = await fetch('https://api.chucknorris.io/jokes/categories');
@@ -9,7 +13,7 @@ const options = createSelect(excludeCategories, queryElement)
   
     // create the select element
     const select = document.createElement("select");
-    select.setAttribute("Id", "category")
+    select.setAttribute("Id", "category");
   
     // create and append the options
     categories.forEach(category => {
@@ -27,26 +31,39 @@ const options = createSelect(excludeCategories, queryElement)
   }
 
 
-  function getChuckNorrisQuote() {
-    
-    const category = document.getElementById("category").value
-     fetch(`https://api.chucknorris.io/jokes/random?category=${category}`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data.value)
-        document.querySelector(".results").textContent = data.value;
-      })
-      .catch(error => console.log(error));
-  }
+let index = 0; // For the anti-spam system
+function getChuckNorrisQuote() {
+    if(index === 0) {
+        const category = document.getElementById("category").value
+        fetch(`https://api.chucknorris.io/jokes/random?category=${category}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.value)
+            document.querySelector(".results").textContent = "";
+            cursor.remove();
+            typewriter(data.value, 0);
+        })
+        .catch(error => console.log(error));
+    }
+}
 
-  
   const button = document.querySelector("button[type=submit]")
   button.addEventListener("click", getChuckNorrisQuote)
 
 
+  function typewriter(text, i) {
+    if(i < text.length) {
+        results.textContent += text[i]
+        index = i;
+        results.appendChild(cursor);
+        setTimeout(() => typewriter(text, i + 1), 50);
+    } else {
+        results.appendChild(cursor);
+        index = 0; // For the anti-spam system
+    }
+}
 
 
-  
 
   
 
